5b17456b25d1a4e7524d8a7230baff44bde14949
edward jenvey <edward_jenvey@yahoo.com>
2014-07-16 13:06:32 -0700

    Revert "Revert "wrote functions""
    This reverts commit 719f091ed306673b97f88c530193d5e18f3d7005.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+#These functions create a real square matrix and then the second returns the inverse.  
+# The inverse is taken from the cache if it has previously been calculated.  Otherwise, it inverts it
 
-## Write a short comment describing this function
 
+#create a real square matrix where one can cache the inverse.
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y){
+      x <<- y
+      inv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(theInv) inv <<- theInv
+  getInv <- function() inv
+  list(set = set, get = get, setInv = setInv, getInv = getInv)
 
 }
 
 
-## Write a short comment describing this function
-
+# get the inverse of x if it is cached.  Otherwise, calculate it and cache it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInv(inv)
+  inv
 }
